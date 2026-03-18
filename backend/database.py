9c6e1e5b36765e147aaaa2dab7f2fe553987c652
Irinida <irinidasuganthi.28ad@licet.ac.in>
2026-03-18 10:56:43 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,10 @@
+from pymongo import MongoClient
 
+# Connect to MongoDB (use localhost in lab, later change to MongoDB Atlas)
+client = MongoClient("mongodb://localhost:27017/")
+db = client["smart_green_delivery"]
+
+orders_collection = db["orders"]
+clusters_collection = db["clusters"]
+
+print("✅ MongoDB connected successfully!")
